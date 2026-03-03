03fb0fec3fb24b1ca2d0ed5a45a8dc99b4773f2f
Murchoid <kamaumkay@gmail.com>
2026-03-03 23:24:40 +0300

    Minor fix on show

--- internal/commands/show.go
+++ internal/commands/show.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"fmt"
 
+	"githum.com/Murchoid/iwashere/internal/domain/models"
 	"githum.com/Murchoid/iwashere/internal/utils"
 )
 
@@ -57,21 +58,8 @@ func (a *ShowCommand) Execute(ctx *Context) error {
 	if err != nil {
 		return err
 	}
-	howLongAgo := utils.HowLongAgo(note.UpdatedAt)
-	fmt.Printf("[%v](%v) %v: %v\n", howLongAgo, note.Branch, note.ID, note.Message)
-	if len(note.Tags) > 0 {
-		fmt.Printf("Tags:[")
-		for idx := range note.Tags {
-			fmt.Printf("%v", note.Tags[idx])
-		}
-		fmt.Printf("]\n")
-	}
-	if len(note.ModifiedFiles) > 0 {
-		fmt.Println("Modified files")
-		for idx := range note.ModifiedFiles {
-			fmt.Printf("[%v]\n", note.ModifiedFiles[idx])
-		}
-	}
+	utils.PrintNotes([]*models.PrivateNote{note}, nil, "short")
+
 	return nil
 }
 
